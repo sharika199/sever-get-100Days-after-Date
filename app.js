ff63b23cc318e4ee0e28cd2cc601e8600554939b
sharika199 <chitturisharika@gmail.com>
2023-07-26 20:08:56 +0000

    sever-get-100Days-after-Date

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const addDays = require("date-fns/addDays");
+
+app.get("/", (request, response) => {
+  const dateTime = new Date();
+  const result = addDays(
+    new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate()),
+    100
+  );
+
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
+  );
+});
+module.exports = app;
